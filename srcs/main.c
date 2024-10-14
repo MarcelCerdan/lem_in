709369a400358d9fcbdb502c9151a847b455625f
mthibaul <maxime.thibault21490@gmail.com>
2024-10-14 11:19:05 +0200

    Init the project

--- srcs/main.c
+++ srcs/main.c
@@ -2,15 +2,17 @@
 #include "lem_in.h"
 
 int main(int ac, char **av) {
-	t_lem_in *lem_in;
+	(void)ac;
+	(void)av;
+	// t_lem_in *lem_in;
 
-	if (ac != 1) {
-		ft_putstr("Usage: ./lem-in < [map]\n");
-		return (1);
-	}
-	lem_in = lem_in_init();
-	lem_in_parse(lem_in);
-	lem_in_solve(lem_in);
-	lem_in_free(lem_in);
+	// if (ac != 1) {
+	// 	ft_putstr("Usage: ./lem-in < [map]\n");
+	// 	return (1);
+	// }
+	// lem_in = lem_in_init();
+	// lem_in_parse(lem_in);
+	// lem_in_solve(lem_in);
+	// lem_in_free(lem_in);
 	return (0);
 }
